02c52b24f8867724203fc4fae8ff46f9ca4e1071
1207537021@qq.com <319421forever>
2021-04-20 21:20:57 +0800

    fix mysql address

--- basic-dynamic-resources/src/main/java/com/wu/basic/dynamic/conf/DynamicConfig.java
+++ basic-dynamic-resources/src/main/java/com/wu/basic/dynamic/conf/DynamicConfig.java
@@ -76,7 +76,7 @@ public class DynamicConfig {
     @Bean(name = "dataSource")
     public DataSource dataSource() {
         MysqlDataSource build = DataSourceBuilder.create().type(MysqlDataSource.class).build();
-        build.setUrl("jdbc:mysql://www.wu2020.top:3306/temp?rewriteBatchedStatements=true&characterEncoding=UTF-8&serverTimezone=Asia/Shanghai");
+        build.setUrl("jdbc:mysql://127.0.0.1:3306/upsert?rewriteBatchedStatements=true&characterEncoding=UTF-8&serverTimezone=Asia/Shanghai");
         build.setUser("root");
         build.setPassword("wujiawei");
         return build;
